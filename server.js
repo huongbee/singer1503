3caa6b2768ecaecb3281663fea1462d9eb1a6cfb
Huong <huongnguyenak96@gmail.com>
2019-04-19 18:10:33 +0700

    config multer

--- server.js
+++ server.js
@@ -2,24 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 const { SingerModel } = require('./models/Singer')
 const bodyParser = require('body-parser')
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-    destination:(req,file, cb)=>{
-        cb(null, '/public/images');
-    },
-    filename: (req,file, cb)=>{
-        cb(null, Date.now() + '-' +file.filename);
-    }
-})
-function fileFilter(req,file, cb){
-    if(file.mimetype == 'image/png' || file.mimetype == 'image/jpeg' || file.mimetype == 'image/jpg')
-        return cb(null,true);
-    return cb(new Error('File not allow!'))
-}
-const upload = multer({
-    storage, fileFilter, limits:{ fileSize:102400}
-})
+const upload = require('./lib/uploadfile.config');
 const app = express();
 
 mongoose.connect('mongodb://localhost/singer1503',{
